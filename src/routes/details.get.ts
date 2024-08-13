27b29c4bba77c681bd2d43d8cee883644e704475
ValDesign <valdesign.dev@gmail.com>
2024-08-13 13:12:22 +0200

    ➕ Added fallback if item is null

--- src/routes/details.get.ts
+++ src/routes/details.get.ts
@@ -1,13 +1,17 @@
 import { Request, Response } from 'express';
-import { searchItemById } from '../utils/video';
+import { getVideoItemById, searchItemById } from '../utils/video';
+import { IMovie, ITvShow, ITvShowEpisode } from '../utils/types';
 
 const detailsHandler = (req: Request, res: Response) => {
   const idQuery = req.query.id;
 
   if (!idQuery) return res.status(400).send('No video path provided');
 
-  const item = searchItemById(parseInt(idQuery as string, 10));
-  if (!item) return res.status(404).send('Video not found');
+  let item: IMovie | ITvShow | ITvShowEpisode | null = searchItemById(parseInt(idQuery as string, 10));
+  if (!item) {
+    item = getVideoItemById(parseInt(idQuery as string, 10));
+    if (!item) return res.status(404).send('Video not found');
+  }
 
   res.status(200).send(item);
 };
