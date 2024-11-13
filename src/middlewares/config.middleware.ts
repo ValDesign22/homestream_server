7012b7e63e322264f588cab3f3395e1599c88f47
ValDesign <valdesign.dev@gmail.com>
2024-11-13 15:56:10 +0100

    fix: Simplify config middleware logic for error handling on PATCH requests

--- src/middlewares/config.middleware.ts
+++ src/middlewares/config.middleware.ts
@@ -5,10 +5,9 @@ import { load_config } from "../utils/config";
 @Injectable()
 export class ConfigMiddleware implements NuxumMiddleware {
   public use(req: Request, res: Response, next: NextFunction) {
-    if (!load_config()) {
-      if (req.path !== '/config' || (req.path === '/config' && req.method !== 'PATCH'))
-        return res.status(500).json({ message: 'No config found' })
+    if (load_config() || (req.path === '/config' && req.method === 'PATCH')) {
+      return next();
     }
-    else next();
+    res.status(500).json({ message: 'No config found' });
   }
 }
