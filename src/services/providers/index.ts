8de4366d6c3568cc84c6e1895b904a6eec8eee71
ValDesign <valdesign.dev@gmail.com>
2025-01-12 10:10:43 +0100

    fix: Remove unused movie search call and standardize interface property formatting

--- src/services/providers/index.ts
+++ src/services/providers/index.ts
@@ -4,7 +4,6 @@ import { tmdb_search } from "./tmdb.service";
 export const search = async (query: string, type: string): Promise<IMovie[] | ITvShow[]> => {
   switch (type) {
     case 'movie':
-      await tmdb_search(query, type);
     case 'tv':
       await tmdb_search(query, type);
     default:
--- src/utils/types/interfaces.util.ts
+++ src/utils/types/interfaces.util.ts
@@ -1,77 +1,77 @@
 import { EMediaType } from './enums.util';
 
 export interface IConfig {
-  files_folder: string,
-  folders: IFolder[],
-  tmdb_language: string,
-  save_images: boolean,
-  create_collections: boolean,
+  files_folder: string;
+  folders: IFolder[];
+  tmdb_language: string;
+  save_images: boolean;
+  create_collections: boolean;
 }
 
 export interface IFolder {
-  id: number,
-  name: string,
-  path: string,
-  generate_trickplay: boolean,
-  media_type: EMediaType,
+  id: number;
+  name: string;
+  path: string;
+  generate_trickplay: boolean;
+  media_type: EMediaType;
 }
 
 export interface IMovie {
-  id: number,
-  collection_id: number,
-  title: string,
-  original_title: string,
-  overview: string,
-  poster_path: string | null,
-  backdrop_path: string | null,
-  logo_path: string | null,
-  release_date: string,
-  runtime: number,
-  genres: IGenre[],
-  path: string | null,
+  id: number;
+  collection_id: number;
+  title: string;
+  original_title: string;
+  overview: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+  logo_path: string | null;
+  release_date: string;
+  runtime: number;
+  genres: IGenre[];
+  path: string | null;
 }
 
 export interface IMovieCollection {
-  id: number,
-  name: string,
-  overview: string,
-  poster_path: string | null,
-  backdrop_path: string | null,
+  id: number;
+  name: string;
+  overview: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
 }
 
 export interface ITvShow {
-  id: number,
-  title: string,
-  original_title: string,
-  overview: string,
-  poster_path: string | null,
-  backdrop_path: string | null,
-  logo_path: string | null,
-  genres: IGenre[],
-  seasons: ITvShowSeason[],
+  id: number;
+  title: string;
+  original_title: string;
+  overview: string;
+  poster_path: string | null;
+  backdrop_path: string | null;
+  logo_path: string | null;
+  genres: IGenre[];
+  seasons: ITvShowSeason[];
 }
 
 export interface ITvShowSeason {
-  id: number,
-  season_number: number,
-  name: string,
-  overview: string,
-  episodes: ITvShowEpisode[],
-  poster_path: string | null,
+  id: number;
+  season_number: number;
+  name: string;
+  overview: string;
+  episodes: ITvShowEpisode[];
+  poster_path: string | null;
 }
 
 export interface ITvShowEpisode {
-  id: number,
-  episode_number: number,
-  title: string,
-  overview: string,
-  air_date: string,
-  still_path: string | null,
-  runtime: number,
-  path: string | null,
+  id: number;
+  episode_number: number;
+  title: string;
+  overview: string;
+  air_date: string;
+  still_path: string | null;
+  runtime: number;
+  path: string | null;
 }
 
 export interface IGenre {
-  id: number,
-  name: string,
+  id: number;
+  name: string;
 }
