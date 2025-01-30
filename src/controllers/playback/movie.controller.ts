9316a1d4c101509e591b43b998e5af3aea207115
ValDesign <valdesign.dev@gmail.com>
2025-01-30 17:52:33 +0100

    feat: Refactor MoviePlaybackController to support HLS streaming and improve directory handling

--- src/controllers/playback/movie.controller.ts
+++ src/controllers/playback/movie.controller.ts
@@ -1,5 +1,5 @@
 import { Controller, Get } from '@nuxum/core';
-import { existsSync, rmdirSync } from 'node:fs';
+import { existsSync, mkdirSync } from 'node:fs';
 import { join } from 'node:path';
 import { Request, Response } from 'express';
 import { get_movie } from '#/services/library/movie.service';
@@ -9,31 +9,25 @@ import { create_hls_stream } from '#/services/stream.service';
 
 @Controller('/playback/movie')
 export class MoviePlaybackController {
-  @Get({
-    query: [
-      {
-        type: 'number',
-        required: true,
-        name: 'id',
-      },
-    ],
-  })
+  @Get('/:id')
   public async get(req: Request, res: Response) {
-    const { id } = req.query;
+    const { id } = req.params;
     const movie_item = get_movie(parseInt(id as string, 10));
     if (!movie_item)
       return res.status(404).json({ message: 'Movie not found' });
 
-    const video_path = movie_item.path;
+    const video_path = movie_item.metadata.path;
     if (!video_path || !existsSync(video_path))
       return res.status(404).json({ message: 'Video file not found' });
 
     const hls_output_dir = join(process.cwd(), HLS_OUTPUT_DIR, `movie_${id}`);
-    const playlist_path = join(hls_output_dir, 'index.m3u8');
+    if (!existsSync(hls_output_dir)) {
+      mkdirSync(hls_output_dir, { recursive: true });
 
-    if (!existsSync(playlist_path)) {
       try {
+        logger.info('Creating HLS stream for movie:', id);
         await create_hls_stream(video_path, hls_output_dir);
+        logger.info('HLS stream created successfully');
       } catch (error) {
         logger.error('Error creating HLS stream:', error);
         return res
@@ -42,11 +36,18 @@ export class MoviePlaybackController {
       }
     }
 
-    res.on('close', () => {
-      logger.info(`Cleaning up HLS files for movie ${id}`);
-      rmdirSync(hls_output_dir, { recursive: true });
-    });
+    res.redirect(`/playback/movie/hls/${id}/720p`);
+  }
 
-    res.sendFile(playlist_path);
+  @Get('/hls/:id/:quality')
+  public async get_hls(req: Request, res: Response) {
+    const { id, quality } = req.params;
+    const hls_output_dir = join(process.cwd(), HLS_OUTPUT_DIR, `movie_${id}`);
+    const playlist_path = join(hls_output_dir, `${quality}.m3u8`);
+    if (!existsSync(playlist_path))
+      return res.status(404).json({ message: 'Quality not found' });
+
+    logger.info('Sending HLS playlist:', playlist_path);
+    return res.sendFile(playlist_path);
   }
 }
--- src/services/stream.service.ts
+++ src/services/stream.service.ts
@@ -1,4 +1,5 @@
 import { exec } from 'node:child_process';
+import { join } from 'node:path';
 
 export const create_hls_stream = async (
   video_path: string,
@@ -7,52 +8,48 @@ export const create_hls_stream = async (
   return new Promise((resolve, reject) => {
     const quality = {
       '360p': {
-        resolution: { width: 640, height: 360 },
+        width: 640,
+        height: 360,
         video_bitrate: '800k',
         audio_bitrate: '96k',
       },
       '480p': {
-        resolution: { width: 842, height: 480 },
+        width: 842,
+        height: 480,
         video_bitrate: '1400k',
         audio_bitrate: '128k',
       },
       '720p': {
-        resolution: { width: 1280, height: 720 },
+        width: 1280,
+        height: 720,
         video_bitrate: '2800k',
         audio_bitrate: '128k',
       },
       '1080p': {
-        resolution: { width: 1920, height: 1080 },
+        width: 1920,
+        height: 1080,
         video_bitrate: '5000k',
         audio_bitrate: '192k',
       },
     };
 
-    const template = (quality: {
-      resolution: { width: number; height: number };
-      video_bitrate: string;
-      audio_bitrate: string;
-    }) =>
-      `-vf scale=w=${quality.resolution.width}:h=${quality.resolution.height}:force_original_aspect_ratio=decrease -c:a aac -ar 48000 -c:v h264 -profile:v main -crf 20 \
-      -sc_threshold 0 -g 48 -keyint_min 48 -hls_time 4 -hls_playlist_type vod \
-      -b:v ${quality.video_bitrate} -maxrate ${parseInt(quality.video_bitrate) + 96}k \
-      -bufsize ${parseInt(quality.video_bitrate) * 1.5}k -b:a ${quality.audio_bitrate} \
-      -hls_segment_filename ${output_path}/${quality.resolution.width}p_%03d.ts ${output_path}/${quality.resolution.width}p.m3u8`;
+    let ffmpegCommand = `ffmpeg -hide_banner -y -i "${video_path}" -preset veryfast -start_number 0 -hls_time 4 -hls_list_size 0`;
 
-    const command = `ffmpeg -hide_banner -y -i "${video_path}" \
-      ${template(quality['360p'])} \
-      ${template(quality['480p'])} \
-      ${template(quality['720p'])} \
-      ${template(quality['1080p'])}`;
+    Object.entries(quality).forEach(([key, q]) => {
+      const outputM3U8 = join(output_path, `${key}.m3u8`);
+      const segmentFile = join(output_path, `${key}_%03d.ts`);
 
-    exec(command, (err, _stdout, _stderr) => {
-      if (err) {
-        reject(err);
+      ffmpegCommand += ` -map v:0 -map a:0 -vf scale=${q.width}:${q.height} -c:v h264 -b:v ${q.video_bitrate} -maxrate ${q.video_bitrate} -bufsize ${parseInt(q.video_bitrate) * 2}k -c:a aac -b:a ${q.audio_bitrate} -hls_segment_filename "${segmentFile}" "${outputM3U8}"`;
+    });
+
+    exec(ffmpegCommand, (error, stdout, stderr) => {
+      if (error) {
+        reject(error);
         return;
       }
 
-      if (_stderr) {
-        reject(_stderr);
+      if (stderr) {
+        reject(stderr);
         return;
       }
 
